777794f0238db42ee8988edc414dfa26400c1e70
Cecilia <gtt201501@gmail.com>
2016-02-09 16:35:07 -0500

    Strings

--- Strings_Anagrams.java
+++ Strings_Anagrams.java
@@ -1,5 +1,29 @@
 package java;
 
-public class Strings_Anagrams {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Strings_Anagrams {
+	static boolean isAnagram(String A, String B) {
+		// Complete the function
+		char[] word1 = A.replaceAll("[\\s]", "").toLowerCase().toCharArray();
+		char[] word2 = B.replaceAll("[\\s]", "").toLowerCase().toCharArray();
+		Arrays.sort(word1);
+		Arrays.sort(word2);
+		return Arrays.equals(word1, word2);
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		boolean ret = isAnagram(A, B);
+		if (ret)
+			System.out.println("Anagrams");
+		else
+			System.out.println("Not Anagrams");
+
+	}
 }
